275a46b35b3539a967e7dfe5c97f12f5d5c5161b
miyad <mhmiyad6565@gmail.com>
2024-05-05 14:12:48 +0600

    making navbar

--- src/components/Navbar/NavComp.jsx
+++ src/components/Navbar/NavComp.jsx
@@ -1,12 +1,28 @@
+"use client";
 import React from "react";
 import DarkMoodBtn from "../DarkMood/DarkMoodBtn";
-
+import { CiSearch } from "react-icons/ci";
+import { CgMonday } from "react-icons/cg";
 const NavComp = () => {
   return (
-    <nav>
-      <div>
-        Logo
-        <DarkMoodBtn />
+    <nav className="py-2 border-b border-b-slate-200 dark:border-b-slate-800 mb-5">
+      <div className="flex justify-between items-center py-2 px-3 bg-white dark:bg-slate-800/5  backdrop-blur-sm max-w-7xl mx-auto">
+        <div>
+          <CgMonday className="size-10 text-slate-900 dark:text-white mx-auto" />
+        </div>
+        <div className="flex gap-2 items-center ">
+          <div className="searchBox">
+            <CiSearch className="mr-2 size-4 shrink-0 opacity-50" />
+            <input
+              type="text"
+              name="search"
+              id="search"
+              className="searchInput"
+              placeholder="Search..."
+            />
+          </div>
+          <DarkMoodBtn />
+        </div>
       </div>
     </nav>
   );
