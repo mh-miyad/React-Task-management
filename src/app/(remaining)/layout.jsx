b56194d4f742fc09d4f366b6d6406bf5e0cc779a
miyad <mhmiyad6565@gmail.com>
2024-05-05 11:36:04 +0600

    add here axios

--- src/app/(remaining)/layout.jsx
+++ src/app/(remaining)/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NextThemeProvider from "@/components/Theme/ThemeProvider";
+import NavComp from "@/components/Navbar/NavComp";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange>
+          <NavComp />
           {children}
         </NextThemeProvider>
       </body>
---
+++ src/components/Navbar/NavComp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import DarkMoodBtn from "../DarkMood/DarkMoodBtn";
+
+const NavComp = () => {
+  return (
+    <nav>
+      <div>
+        Logo
+        <DarkMoodBtn />
+      </div>
+    </nav>
+  );
+};
+
+export default NavComp;
