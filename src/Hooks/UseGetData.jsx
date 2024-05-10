b19fdb87e3bbf3bbd942f1e0fb57cc637afa456e
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-05-10 07:23:37 +0600

    get set dat url

--- src/Hooks/UseGetData.jsx
+++ src/Hooks/UseGetData.jsx
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 const UseGetData = () => {
+  const baseUrl =
+    "https://react-task-management-weld.vercel.app/api/addtask" ||
+    "http://localhost:3000";
   const { isPending, error, data } = useQuery({
     queryKey: ["repoData"], // Unique identifier for your query
     queryFn: async () => {
-      const response = await axios.get("http://localhost:3000/api/addtask");
+      const response = await axios.get(`${baseUrl}/api/addtask`);
       return response.data;
     },
   });
